5b2e9668e499f02fadc5360412b53fcf792cca0f
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:24:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-wildflower-33",
+  url: "https://twilight-wildflower-33.botics.co",
+  api: "https://twilight-wildflower-33.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
